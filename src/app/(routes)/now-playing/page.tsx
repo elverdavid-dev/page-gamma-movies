70da948d8cd39c4800fad2758bc2b23f353b543a
Elver David <elverdavid0839@gmail.com>
2024-08-09 14:22:41 -0500

    feat(upcoming): add upcoming page

--- src/app/(routes)/now-playing/page.tsx
+++ src/app/(routes)/now-playing/page.tsx
@@ -1,11 +1,15 @@
-import MoviesGrid from "@/modules/core/components/movies/movies-grid"
-import { getNowPlayingMovies } from "@/modules/home/services/get-now-playing-movies"
+import MoviesGrid from '@/modules/core/components/movies/movies-grid'
+import { getNowPlayingMovies } from '@/modules/home/services/get-now-playing-movies'
 
 const NowPlayingPage = async () => {
-  const movies = await getNowPlayingMovies()
-  return (
-    <MoviesGrid movies={movies?.results ?? []} title="Now Playing" totalResults={movies?.total_pages ?? 0} />
-  )
+	const movies = await getNowPlayingMovies()
+	return (
+		<MoviesGrid
+			movies={movies?.results ?? []}
+			title="Now Playing"
+			totalResults={movies?.total_results ?? 0}
+		/>
+	)
 }
 
 export default NowPlayingPage
--- src/app/(routes)/popular/page.tsx
+++ src/app/(routes)/popular/page.tsx
@@ -2,15 +2,15 @@ import MoviesGrid from '@/modules/core/components/movies/movies-grid'
 import { getPopularMovies } from '@/modules/home/services/get-popular-movies'
 
 const PopularPage = async () => {
-  const movies = await getPopularMovies()
+	const movies = await getPopularMovies()
 
-  return (
-    <MoviesGrid
-      movies={movies?.results ?? []}
-      title="Popular"
-      totalResults={movies?.total_results ?? 0}
-    />
-  )
+	return (
+		<MoviesGrid
+			movies={movies?.results ?? []}
+			title="Popular"
+			totalResults={movies?.total_results ?? 0}
+		/>
+	)
 }
 
 export default PopularPage

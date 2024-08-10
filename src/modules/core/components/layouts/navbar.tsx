e2c71206f548fac6da910e6d56eae94f4e606cca
Elver David <elverdavid0839@gmail.com>
2024-08-10 07:50:32 -0500

    refactor(ui): refactor in navbar

--- src/modules/core/components/layouts/navbar.tsx
+++ src/modules/core/components/layouts/navbar.tsx
@@ -2,6 +2,7 @@
 import Logo from '@/modules/core/components/common/logo'
 import SignInButton from '@/modules/core/components/layouts/header/sign-in-button'
 import {
+	cn,
 	Navbar,
 	NavbarBrand,
 	NavbarContent,
@@ -11,17 +12,19 @@ import {
 	NavbarMenuToggle,
 } from '@nextui-org/react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { useState } from 'react'
 
 const Navbar2 = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false)
+	const pathName = usePathname()
 	const navLinks = [
 		{ name: 'Popular', path: '/popular' },
 		{ name: 'Now Playing', path: '/now-playing' },
 		{ name: 'Upcoming', path: '/upcoming' },
 		{ name: 'Top Rated', path: '/top-rated' },
-		{ name: 'Favorite', path: '/' },
-		{ name: 'Saved', path: '/' },
+		{ name: 'Favorite', path: '/favorite' },
+		{ name: 'Saved', path: '/saved' },
 	]
 	return (
 		<Navbar
@@ -30,15 +33,29 @@ const Navbar2 = () => {
 			maxWidth="full"
 			className="bg-black py-2"
 		>
-			<NavbarBrand>
-				<Logo />
-			</NavbarBrand>
+			{/* Toggle */}
+			<NavbarContent className="lg:hidden" justify="start">
+				<NavbarMenuToggle
+					className="text-white"
+					aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+				/>
+			</NavbarContent>
+			<NavbarContent justify="center">
+				<NavbarBrand>
+					<Logo />
+				</NavbarBrand>
+			</NavbarContent>
+
+			{/* Desktop navbar items */}
 			<NavbarContent className="hidden lg:flex" justify="start">
 				{navLinks.map(({ name, path }) => (
-					<NavbarItem key={`menuItem-${name}`}>
+					<NavbarItem key={`menuItem-${name}`} isActive={pathName === path}>
 						<Link
 							href={path}
-							className="text-gray-300 text-lg md:text-base hover:text-white transition-all"
+							className={cn(
+								'text-gray-300 text-lg md:text-base hover:text-white transition-all',
+								pathName === path && 'text-[#F0B90B] hover:text-[#F0B90B]',
+							)}
 						>
 							{name}
 						</Link>
@@ -46,19 +63,14 @@ const Navbar2 = () => {
 				))}
 			</NavbarContent>
 
-			<NavbarContent className="hidden lg:flex" justify="end">
+			<NavbarContent className="lg:items-center lg:flex" justify="end">
 				<NavbarItem>
 					<SignInButton />
 				</NavbarItem>
 			</NavbarContent>
-			{/* Toggle */}
-			<NavbarContent className="lg:hidden" justify="end">
-				<NavbarMenuToggle
-					className="text-white"
-					aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-				/>
-			</NavbarContent>
-			<NavbarMenu className="bg-black pt-5 flex flex-col gap-y-5 items-center">
+
+			{/* Mobile navbar items */}
+			<NavbarMenu className="bg-black pt-5 flex flex-col gap-y-5 items-center z-50">
 				{navLinks.map(({ name, path }) => (
 					<NavbarMenuItem key={`menuItem-${name}`}>
 						<Link
@@ -69,7 +81,6 @@ const Navbar2 = () => {
 						</Link>
 					</NavbarMenuItem>
 				))}
-				<SignInButton />
 			</NavbarMenu>
 		</Navbar>
 	)

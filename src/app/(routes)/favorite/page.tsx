6ff6f225b92c35c75aa49a724ee64f98ac04f933
Elver David <elverdavid0839@gmail.com>
2024-08-19 16:54:02 -0500

    feat(favorite): add favorite container component

--- src/app/(routes)/favorite/page.tsx
+++ src/app/(routes)/favorite/page.tsx
@@ -1,10 +1,11 @@
 import FavoriteContainer from '@/modules/favorite/components/favorite-container'
+import MoviesGridSkeleton from '@/modules/shared/components/movies/skeletons/movies-grid-skeleton'
 import { Suspense } from 'react'
 
 const FavoritePage = async () => {
 	return (
 		<>
-			<Suspense fallback={<span>loading...</span>}>
+			<Suspense fallback={<MoviesGridSkeleton label='Favorite' />}>
 				<FavoriteContainer />
 			</Suspense>
 		</>
